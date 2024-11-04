28093a026f8531eb0131ca9f4ec253b8c98857cf
mmontoya <marcelomontoyaflores2511@gmail.com>
2024-11-03 21:43:44 -0500

    Update archivo hola_mundo.py en la rama Development

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,5 +3,8 @@ def saludo():
     print("Hola, mundo!")
     print("SESION 2 DE GIT CAMBIOS EN EL REPOSITORIO REMOTO")
 
+    print("SESION 2 DE GIT CAMBIOS REALIZADOS EN EL BRANCH - OJO EN EL BRANCH")
+
+
 if __name__ == "__main__":
     saludo()

86173706d76a7d5599e874878d899ed3748d25e0
mmontoya <marcelomontoyaflores2511@gmail.com>
2024-11-04 10:04:00 -0500

    nueva tarea

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -1,6 +1,6 @@
 # gestor_tareas.py
 
-tareas=["Estudiar GIT", "Completar el trabjo de clase"]
+tareas=["Estudiar GIT", "Completar el trabjo de clase", "Enviar al correo el repositorio de github"]
 
 def listar_tareas(): 
 	for tarea in tareas:
